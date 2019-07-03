5db82f87c10badb9e3096e7ef0815fbd196f705c
tuckerrrrrrrrrrrr <tucker1014@hotmail.com>
2019-07-02 20:06:50 -0700

    organization time

--- HSlider.gd
+++
@@ -1,7 +0,0 @@
-extends HSlider
-
-func _ready():
-	pass # Replace with function body.
-
-func _process(delta):
-	get_node("../../screen").material.set_shader_param("percent", value)
--- human.gd
+++
@@ -1,7 +0,0 @@
-extends Sprite
-
-onready var ghost : Node2D = get_node("../../../ghost/Viewport/player")
-
-func _process(delta):
-	position = ghost.position
-	flip_h = ghost.get_node("sprite").flip_h
