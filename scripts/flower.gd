2744b03704333b4f645d53a0b3af6da7719718d2
tuckerrrrrrrrrrrr <tucker1014@hotmail.com>
2019-09-04 18:48:54 -0700

    delete crushed flower

--- scripts/flower.gd
+++ scripts/flower.gd
@@ -8,5 +8,5 @@ func _ready():
 func _on_die_body_entered(body):
 	if body.name == "box":
 		yield(get_tree().create_timer(0.2), "timeout")
-		visible = false
 		emit_signal("flower_death")
+		queue_free()
